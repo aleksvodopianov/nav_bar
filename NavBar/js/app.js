f7377f9455bd8f72b5e68d587ef1f2531f39ad46
Aleks <vodopyanov3d@gmail.com>
2022-05-09 03:22:57 +0300

    created layout

---
+++ NavBar/js/app.js
@@ -0,0 +1,7 @@
+//Импортируем все функции из файла functions.js и присваиваем им общее имя flsFunctions
+import * as flsFunctions from "./modules/functions.min.js"; 
+
+//Получаем доступ к файлу functions.js и вызываем из него функцию
+flsFunctions.isWebp();
+
+
